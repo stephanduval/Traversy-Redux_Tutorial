852372fbdbe94a18e9c2d94431882cb03998db2c
Stephan DuVal <stephan.duval@gmail.com>
2021-06-29 17:29:04 -0600

    installed Babel/core

--- src/App.js
+++ src/App.js
@@ -1,29 +1,41 @@
+//installions:
+//npm install redux, redux-react, redux-thunk
+// npm install --save redux
+// npm install @babel/core @babel/preset-env
+
+
 import logo from './logo.svg';
 import './App.css';
+import { provider } from 'react-redux';
+
+
 import Posts from './components/post'
 import PostsForm from './components/postform'
 
+
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-      <PostsForm />
-      <hr></hr>
-      <Posts />
-    </div>
+
+      <div className="App">
+        <header className="App-header">
+          <img src={logo} className="App-logo" alt="logo" />
+          <p>
+            Edit <code>src/App.js</code> and save to reload.
+          </p>
+          <a
+            className="App-link"
+            href="https://reactjs.org"
+            target="_blank"
+            rel="noopener noreferrer"
+          >
+            Learn React
+          </a>
+        </header>
+        <PostsForm />
+        <hr></hr>
+        <Posts />
+      </div>
+
   );
 }
 
